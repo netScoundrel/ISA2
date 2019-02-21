df46e6c2e2882b4f89d833124ac60d6fcb19242b
netScoundrel <estfrom@gmail.com>
2019-02-21 17:20:22 +0200

    .

---
+++ Week4/Labor/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Labor.Models;
+
+namespace Labor.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult GetView()
+        {
+            Employee emp = new Employee();
+            emp.FirstName = "Paul";
+            emp.LastName = "Opmann";
+            emp.Salary = 1510;
+            return View("MyView", emp);
+        }
+    }
+}
