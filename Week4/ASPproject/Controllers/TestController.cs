824db21f52987413a5821e48cf83e82a02a23849
netScoundrel <estfrom@gmail.com>
2019-02-21 14:23:29 +0200

    adding view

---
+++ Week4/ASPproject/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPproject.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult GetView()
+        {
+            return View("MyView");
+        }
+    }
+}
