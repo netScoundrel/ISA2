19e4cdd9d065bba3b2e826ca798198f61c3300e0
netScoundrel <estfrom@gmail.com>
2019-02-24 18:31:21 +0200

    Rules for username and passwords are added

--- Week4/Core/UserDetails.cs
+++ Week4/Core/UserDetails.cs
@@ -6,6 +6,7 @@ namespace Core
 {
     public class UserDetails
     {
+        [UserValidation]
         public string UserName { get; set; }
         public string Password { get; set; }
     }
---
+++ Week4/Core/UserValidation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Core
+{
+    class UserValidation : NameValidation
+    {
+        protected const string requiredLength = "username lenght should be between 2 and 7";
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null) return error(requiredLength);
+            var s = value.ToString();
+            if (s.Length < 2) return error(requiredLength);
+            if (s.Length > 7) return error(requiredLength);
+            if (!onlyLetters(s)) return error(useOnlyLetters);
+            return ValidationResult.Success;
+        }
+    }
+}
