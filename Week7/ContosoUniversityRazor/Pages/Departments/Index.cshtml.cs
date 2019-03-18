6688c03b0f8be46cd699f5a65aa45a0c9ef1bea8
netScoundrel <estfrom@gmail.com>
2019-03-18 14:02:09 +0200

    Concurrency

---
+++ Week7/ContosoUniversityRazor/Pages/Departments/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversityRazor.Data;
+using ContosoUniversityRazor.Models;
+
+namespace ContosoUniversityRazor.Pages.Departments
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversityRazor.Data.SchoolContext _context;
+
+        public IndexModel(ContosoUniversityRazor.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Department> Department { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Department = await _context.Departments
+                .Include(d => d.Administrator).ToListAsync();
+        }
+    }
+}
